d8d321d61af0582bbe68e9007ca420aea33fcbdf
RyanMikhael <ryanmikhael20@gmail.com>
2025-09-10 23:20:33 -0300

    feat: creating home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:volei_app/pages/draw_teams_page.dart';
+import 'package:volei_app/pages/list_players_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> tabs = [const ListPlayersPage(), const DrawTeamsPage()];
+
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: tabs[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt),
+            label: 'Jogadores',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.double_arrow_rounded), label: 'Sortear times')
+        ],
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+      ),
+    );
+  }
+}
