3b6bdedc203f1bd71c9a43eae7b6199897ac3a2c
RyanMikhael <ryanmikhael20@gmail.com>
2025-09-12 14:24:07 -0300

    add player model

---
+++ lib/model/player.dart
@@ -0,0 +1,12 @@
+class Player {
+  int id;
+  String name;
+  int level;
+  String position;
+
+  Player(
+      {required this.id,
+      required this.name,
+      required this.level,
+      required this.position});
+}
